ae77600c3ad6a649526daf9a2b4aa2d737b02c37
Oguzhan Ipek <fgtyhtyh@hotmail.com>
2023-03-12 23:06:10 +0300

    temp changes

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,26 +1,26 @@
-use http::{Request, StatusCode};
-use hyper::Body;
-use tonsail_server::{
-    prisma::PrismaClient,
-    routes::{create_router, AppState},
-};
-use tower::ServiceExt;
-
-#[tokio::test]
-async fn returns_200_when_application_is_healthy() {
-    let (client, _mock) = PrismaClient::_mock();
-    let state = AppState::new(client);
-    let app = create_router(state);
-
-    let response = app
-        .oneshot(
-            Request::builder()
-                .uri("/health_check")
-                .body(Body::empty())
-                .unwrap(),
-        )
-        .await
-        .unwrap();
-
-    assert_eq!(response.status(), StatusCode::OK);
-}
+// use http::{Request, StatusCode};
+// use hyper::Body;
+// use tonsail_server::{
+//     prisma::PrismaClient,
+//     routes::{create_router, AppState},
+// };
+// use tower::ServiceExt;
+//
+// #[tokio::test]
+// async fn returns_200_when_application_is_healthy() {
+// let (client, _mock) = PrismaClient::_mock();
+//     let state = AppState::new(client);
+//     let app = create_router(state);
+//
+//     let response = app
+//         .oneshot(
+//             Request::builder()
+//                 .uri("/health_check")
+//                 .body(Body::empty())
+//                 .unwrap(),
+//         )
+//         .await
+//         .unwrap();
+//
+//     assert_eq!(response.status(), StatusCode::OK);
+// }
